a01e2b21e436a0219f1f1eaf747e8616cdb66bf2
Nicolas S <soncininicolas@gmail.com>
2017-05-15 11:01:56 -0300

    Prototype of some system calls

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -47,11 +47,61 @@
 void
 ExceptionHandler(ExceptionType which)
 {
-    int type = machine->ReadRegister(2);
 
-    if (which == SYSCALL_EXCEPTION && type == SC_Halt) {
-        DEBUG('a', "Shutdown, initiated by user program.\n");
-        interrupt->Halt();
+    if (which == SYSCALL_EXCEPTION) {
+        int type = machine->ReadRegister(2);
+        switch(type){
+            case SC_Halt:
+                DEBUG('a', "Shutdown, initiated by user program.\n");
+                interrupt->Halt();
+                break;
+            case SC_Create:
+                int pname = machine->ReadRegister(4);
+                char name[128];
+                //Create the file
+                ReadStringFromUser(pname, name, 128); //Releer la implementacion y ver si funciona cuando corta por exceso de caracteres y si deja un EOF
+                filesys->Create(name,0);
+                //Increase the PC
+                machine->WriteRegister(PREV_PC_REG, machine->ReadRegister(PC_REG);
+                machine->WriteRegister(PC_REG, machine->ReadRegister(PC_REG)+4);
+                machine->WriteRegister(NEXT_PC_REG, machine->ReadRegister(PC_REG)+4); //FIXME?
+                break;
+            case SC_Read:
+                int pbuf = machine->ReadRegister(4);
+                int size = machine->ReadRegister(5);
+                OpenFileId id = machine->ReadRegister(6);
+                //Obtain file from filesystem
+                filesys //Check if fileid exists and what file it corresponds to
+                //Read the file
+                ReadBufferFromUser(/*TODO*/, pbuf, size); //TODO: If file is not long enough...?!
+                //Return how much was read
+                machine->WriteRegister(2, /*TODO*/);
+                //Increase the PC
+                /*TODO*/
+                break;
+            case SC_Write:
+                break;
+            case SC_Open:
+                int pname = machine->ReadRegister(4);
+                char name[128];
+                ReadStringFromUser(pname, name, 128);
+                //Open the file
+                OpenFile *f = filesys->Open(name);
+                if(f == NULL) {/*TODO*/}
+                OpenFileId fid = /*TODO: Crear la funcion que asocia y crea un file id*/
+                //Return the fileid
+                machine->WriteRegister(2, fid);
+                //Increase the PC
+                /*TODO*/
+                break;
+            case SC_Close:
+                break;
+            case SC_Exit:
+                break;
+            case SC_Join:
+                break;
+            case SC_Exec:
+                break;
     } else {
         printf("Unexpected user mode exception %d %d\n", which, type);
         ASSERT(false);
