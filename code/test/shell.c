424594a00e70ca4a3ee1eeb07c6db71d0c28c034
Nicolas S <soncininicolas@gmail.com>
2017-06-20 16:11:14 -0300

    Polished and finished(ish) console implementation

--- code/test/shell.c
+++ code/test/shell.c
@@ -1,69 +1,76 @@
 #include "syscall.h"
+#include <stdbool.h>
 
 
 #define MAX_LINE_SIZE  60
 #define MAX_ARG_COUNT  32
-#define ARG_SEPARATOR  ' '
+
+#define ARG_SEPARATOR        ' '
+#define ARG_AGGREGATOR_START '('
+#define ARG_AGGREGATOR_END   ')'
+#define BACKGROUND_RUN       '&'
 
 #define NULL  ((void *) 0)
 
 static inline unsigned
 strlen(const char *s)
 {
-    // TO DO: how to make sure that `s` is not `NULL`?
-
-    unsigned i;
-    for (i = 0; s[i] != '\0'; i++);
+    unsigned i = 0;
+    if(s != NULL){
+        for (i = 0; s[i] != '\0'; i++);
+    }
     return i;
 }
 
 static inline void
 WritePrompt(OpenFileId output)
 {
-    static const char PROMPT[] = "--> ";
-    Write(PROMPT, sizeof PROMPT - 1, output);
+    static const char PROMPT[] = "nachos$ ";
+    Write(PROMPT, sizeof(PROMPT) - 1, output);
 }
 
 static inline void
 WriteError(const char *description, OpenFileId output)
 {
-    // TO DO: how to make sure that `description` is not `NULL`?
-
-    static const char PREFIX[] = "Error: ";
+    static const char PREFIX[] = "(nachos)Error: ";
     static const char SUFFIX[] = "\n";
 
-    Write(PREFIX, sizeof PREFIX - 1, output);
-    Write(description, strlen(description), output);
-    Write(SUFFIX, sizeof SUFFIX - 1, output);
+    if(description != NULL){
+        Write(PREFIX, sizeof PREFIX - 1, output);
+        Write(description, strlen(description), output);
+        Write(SUFFIX, sizeof SUFFIX - 1, output);
+    }
 }
 
 static unsigned
 ReadLine(char *buffer, unsigned size, OpenFileId input)
 {
-    // TO DO: how to make sure that `buffer` is not `NULL`?
-
     unsigned i;
 
-    for (i = 0; i < size; i++) {
-        Read(&buffer[i], 1, input);
-        // TO DO: what happens when the input ends?
-        if (buffer[i] == '\n') {
-            buffer[i] = '\0';
-            break;
+    if(buffer != NULL){
+        for (i = 0; i < size-1; i++) {
+            Read(&buffer[i], 1, input);
+            if((buffer[i] == '\n') || (buffer[i] == '\0')){
+                i--;
+                break;
+            }
         }
+        buffer[i+1] = '\0';
     }
     return i;
 }
 
 static int
-PrepareArguments(char *line, char **argv, unsigned argvSize)
+PrepareArguments(char *line, char **argv, unsigned argvSize, bool *bg)
 {
-    // TO DO: how to make sure that `line` and `argv` are not `NULL`?, and
+    // TODO: how to make sure that `line` and `argv` are not `NULL`?, and
     //        for `argvSize`, what precondition should be fulfilled?
     //
     // PENDIENTE: use `bool` instead of `int` as return type; for doing this,
     //            given that we are in C and not C++, it is convenient to
     //            include `stdbool.h`.
+    // TODO(nico): chequear que el primer char sea BACKGROUND_RUN y poner
+    // en true o false bg correspondientemente (y borrar el char si esta)
 
     unsigned argCount;
 
@@ -106,28 +113,29 @@ main(void)
     for (;;) {
         WritePrompt(OUTPUT);
         const unsigned lineSize = ReadLine(line, MAX_LINE_SIZE, INPUT);
-        if (lineSize == 0)
+        if (lineSize == 0){
             continue;
+        }
 
-        if (PrepareArguments(line, argv, MAX_ARG_COUNT) == 0) {
+        bool bg;
+        if (PrepareArguments(line, argv, MAX_ARG_COUNT, &bg) == 0) {
             WriteError("too many arguments.", OUTPUT);
             continue;
         }
 
-        // Comment and uncomment according to whether command line arguments
-        // are given in the system call or not.
-        const SpaceId newProc = Exec(line);
-        //const SpaceId newProc = Exec(line, argv);
+        const SpaceId newProc = Exec(line, argv);
+        if(SpaceId < 0){ //FIXME if needed (after implementing pids)
+            WriteError("error executing the process.", OUTPUT);
+        }
 
-        // TO DO: check for errors when calling `Exec`; this depends on how
-        //        errors are reported.
-
-        Join(newProc);
-        // TO DO: is it necessary to check for errors after `Join` too, or
+        if(!bg){
+            Join(newProc);
+        // TODO: is it necessary to check for errors after `Join` too, or
         //        can you be sure that, with the implementation of the system
         //        call handler you made, it will never give an error?; what
         //        happens if tomorrow the implementation changes and new
         //        error conditions appear?
+        }
     }
 
     return 0;  // Never reached.
