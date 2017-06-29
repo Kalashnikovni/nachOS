2212156eef752830d74bccefee9db3d00b626db6
Kalashnikovni <denise.marzorati@gmail.com>
2017-06-29 12:23:18 -0300

    Small fixes

--- code/test/cat.c
+++ code/test/cat.c
@@ -8,10 +8,13 @@ main(int argc, char **argv)
     int i;
     for(i = 1; i < argc; i++){
         OpenFileId fid = Open(argv[i]);
-        char c;
-        while(Read(&c, 1, fid) != 0){
-            Write(&c, 1, ConsoleOutput);
+        if(fid != -1){
+            char c;
+            while(Read(&c, 1, fid) != 0){
+                Write(&c, 1, ConsoleOutput);
+            }
         }
+        Close(argv[i]);
     }
 
     Exit(0);
