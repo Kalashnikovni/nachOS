bffaed870d02ac4210f6e051e75d7879a957f84b
Nicolas S <soncininicolas@gmail.com>
2017-07-31 17:34:33 -0300

    Moved coremap (add with last commit)

---
+++ code/vmem/coremap.cc
@@ -0,0 +1,32 @@
+// COREMAP definitions
+//
+
+#include "coremap.hh"
+
+Coremap::Coremap(int n) : BitMap(n)
+{
+    nitems = n;
+    nextVictim = -1;
+}
+
+int
+Coremap::Find(AddressSpace *own, int vpn)
+{
+    int free = BitMap::Find();
+    if(free == -1){ //Push victim to swap and take its place
+        int victim = SelectVictim();
+        ASSERT((0 <= victim) && (victim < nitems));
+        owner[victim] -> SaveToSwap(VPN[victim]);
+        free = victim;
+    }
+    owner[free] = own;
+    VPN[free] = vpn;
+    return free;
+}
+
+int
+Coremap::SelectVictim()
+{
+    nextVictim = ++nextVictim % nitems;
+    return nextVictim;
+}
